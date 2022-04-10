2ad1dbeb3316f5122963b9d03844798a2adb863d
manasvivaghasiya <manasvivaghasiya2002@gmail.com>
2022-04-10 21:20:25 +0530

    end

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment';
-import { FormBuilder, FormGroup } from '@angular/forms';
-// import { debug } from 'console';
 
 
 @Component({
@@ -12,157 +10,128 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  showModal: any
-  closeResult = '';
-  studentList:any;
-  studentData!:FormGroup;
-  editInfo!: any;
-  studentId: any;
-  hobbies = ["sports", "playing", "music", "reading"];
-  studentInfo: any;
-  constructor(private modalService: NgbModal,private http:HttpClient,  private fb: FormBuilder,) { }
+  userForm! : FormGroup
+userData : any = []
+// userhobbies : any = []
+userHobbies : any = []
+  constructor(private fb : FormBuilder , private httpclient : HttpClient) {
+    this.userForm = this.fb.group({
+      firstName : ['', Validators.required],
+      lastName : ['', Validators.required],
+      gender : [''],
+      age : ['', Validators.required],
+      city : [''],
+      id : []
+    });
+   }
 
   ngOnInit(): void {
-  this.getStudent()
-  this.studentData= this.fb.group({
-    firstName:[''],
-    lastName:[''],
-    // hobbies:[''],
-    hobbies   : this.fb.array(["sports","playing"]),
-    gender:[''],
-    city:[''],
-    age:[''],
-
-
-  });
-
+   this.getdetails()
   }
-  open(content: any) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
+  // hobbies
+  hobbiesChanged($event : any){
+    const value = $event.target.value;
+    if(this.userHobbies.find((a: any)=> a === value)){
+     this.userHobbies = this.userHobbies.filter((a: any)=> a !== value)
+    }
+    else{
+      this.userHobbies.push(value)
     }
   }
-
-  onSubmit(){
-
+ 
+// save data
+saveData(){
+  if(this.userForm.value.id){
+    this.updateData();
   }
-  getStudent(){
-    this.http.get(`${environment.apiProduct}/student/get`).subscribe((res:any)=>{
-      this.studentList = res.data
-    })
+  else{
+    this.addData();
   }
-
-  deleteStudent(id:string){
-    this.http.delete(`${environment.apiProduct}/student/delete?id=${id}`)
-    .subscribe((res:any)=>{
-     this.studentInfo = null
-        alert('data SuccessFully delete')
-        this.getStudent()
-    });
-  }
-
-  addStudent(){
-    debugger
-    if(this.studentInfo){
-      this.updateStudent()
-    }
-     this.http.post(`${environment.apiProduct}/student/add`,this.studentData.value)
-     .subscribe((res:any)=>{
-      alert('data successfully add');
-     this.getStudent();
-     this.studentData.reset();
-     
-    });
-  
-  }
-
-  updateStudent(){
-    debugger
-      this.http.post(`${environment.apiProduct}/student/update`,{
-        ...this.studentInfo,
-        id:this.studentInfo.id,
-        ...this.studentData.value,
-      })
-    .subscribe((res:any)=>{
-      this.studentInfo=null
-      alert('data Successfully update');
-      this.getStudent();
-      this.studentData.reset();
-    });
-  }
-
-  // editStudent(dataStudent:any){
-  //   this.editInfo = this.studentData
-  //   this.studentData = dataStudent
-  // }
-
-  // editStudent(data:any){
-  //   debugger
-  //  let firstName = this.studentData.get('firstName')?.value
-  // this.studentId = data.id;
-  // this.studentData.value.get ('firstName').setValue(data.firstName);
-  // let firstName = this.studentData.value.get('firstName')?.value;
-  //  this.studentData.value.setValue(firstName);
-  // }
-
-  editStudent(dataStudent:any){
-    // this.studentInfo = null;
-    debugger
-    this.studentInfo = dataStudent
-    console.log(this.studentList);
-    var result = this.studentList.filter((x: { _id: any; }) => x._id == this.studentInfo);
-    console.log(result);
-    let first_name = result[0].firstName;
-    let last_name = result[0].lastName;
-    let hobbies_name = result[0].hobbies;
-    let gender_name = result[0].gender;
-    let city_name = result[0].city;
-    let age = result[0].age;
-    // console.log(first_name);
-    console.log(hobbies_name,age,gender_name);
-    // let first_name = this.studentData.get('firstName')?.value;
-    // let last_name = this.studentData.get('lastName')?.value;
-    this.studentData.patchValue({
-      firstName:first_name,
-      lastName:last_name,
-      hobbies:hobbies_name,
-      gender:gender_name,
-      city:city_name,
-      age:age
-
-      
-      // lastName:dataStudent.lastName,
-      // hobbies:dataStudent.hobbies,
-      // gender:dataStudent.gender,
-      // city:dataStudent.city,
-      // age:dataStudent.age,
-
- })
-  // this.studentData.value.get ('firstName').setValue(dataStudent.firstName);
-  // this.studentData.value.setValue(dataStudent.firstName);
-
+  if(!this.userForm.valid){
+    return
   }
 }
 
-// export interface dataStudent{
-//   firstName:string;
-//   lastName:string;
-//   hobbies:string;
-//   gender:string;
-//   city:string;
-//   age:string;
-//   _id:string;
-// }
+// get details
+getdetails(){
+  this.httpclient.get(`${environment.apiProduct}/student/get`)
+  .subscribe((res:any)=>{
+    this.userData = res.data
+  });
+}
 
+// editData
+editData(payload:any){
+  this.userForm.patchValue({
+    firstName : payload.firstName,
+    lastName : payload.lastName,
+    gender : payload.gender,
+    age : payload.age,
+    city : payload.city,
+    id : payload._id,
+  });
+  if(payload.hobbies){
+    this.userHobbies = payload.hobbies.split(',')
+  }
+}
+
+// add data
+addData(){
+  const payload = {
+    "firstName" : this.userForm.value.firstName,
+    "lastName" : this.userForm.value.lastName,
+    "gender" : this.userForm.value.gender,
+    "age" : this.userForm.value.age,
+    "city" : this.userForm.value.city,
+    "id" : this.userForm.value.id,
+    "hobbies" : this.userHobbies.join(',')
+  }
+  this.httpclient.post(`${environment.apiProduct}/student/add`,payload)
+  .subscribe((res:any)=>{
+    if(res.isSuccess){
+      this.getdetails()
+      this.userForm.reset()
+      this.userHobbies = []
+    }
+    else{
+      alert(res.message)
+    }
+  });
+}
+//btnclick
+btnclick(){
+  
+}
+// update data
+updateData(){
+  const payload = {
+    "firstName" : this.userForm.value.firstName,
+    "lastName" : this.userForm.value.lastName,
+    "gender" : this.userForm.value.gender,
+    "age" : this.userForm.value.age,
+    "city" : this.userForm.value.city,
+    "id" : this.userForm.value.id,
+    "hobbies" : this.userHobbies.join(',')
+  }
+  this.httpclient.post(`${environment.apiProduct}/student/update`,payload)
+  .subscribe((res:any)=>{
+    if(res.isSuccess){
+      this.getdetails()
+      this.userForm.reset()
+      this.userHobbies = []
+    }
+    else{
+      alert(res.message)
+    }
+  });
+}
+
+
+// deleteData
+deleteData(id: any){
+  this.httpclient.delete(`${environment.apiProduct}/student/delete?id=${id}`)
+  .subscribe((res:any)=> {
+    this.getdetails();
+  });
+}
+}
